bb15df768cb1fab82ff2996770079006b190e092
Mohammed desktop <mohammed.alsalhi@outlook.com>
2024-10-12 15:20:51 +0400

    This is sport website project

---
+++ src/test/java/Mohammed/salim/Sport/SportApplicationTests.java
@@ -0,0 +1,13 @@
+package Mohammed.salim.Sport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
